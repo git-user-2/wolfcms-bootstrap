012bd48261bb138c9fbb974e8efcbc429070dc62
Kyle Jones <whabash090@gmail.com>
2013-09-03 22:22:02 -0700

    attempt to fix mobile scaling issue

--- bs_responsive_layout.php
+++ bs_responsive_layout.php
@@ -8,6 +8,7 @@
   <meta name="description" content="<?php echo ($this->description() != '') ? $this->description() : 'Default description goes here'; ?>" />
   <meta name="keywords" content="<?php echo ($this->keywords() != '') ? $this->keywords() : 'default, keywords, here'; ?>" />
   <meta name="author" content="Author Name" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Bootstrap Default Theme -->
   <link href="<?php echo THEMES_URI; ?>wolfcms-bootstrap/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
